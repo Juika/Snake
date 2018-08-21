2a6fb44eff53782747c3bff9d4f10c299111970c
Vlad <zilkov@hotmail.com>
2018-08-21 09:36:30 +0300

    Initial commit

---
+++ Snake/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public Int32 x,y;
+        public char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Point p1 = new Point(7, 2, '*');
+
+            Point p2 = new Point(2,3,'#');
+            
+            p1.Draw();
+
+            p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(3);
+            numList.Add(5);
+            numList.Add(7);
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            int x = numList[0];
+
+            Console.ReadLine();
+        }
+        static void Draw(Point p)
+        {
+            Console.SetCursorPosition(p.x, p.y);
+            Console.Write(p.sym);
+        }
+    }
+}
